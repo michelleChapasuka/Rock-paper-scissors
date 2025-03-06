b8afce1ca911faf1a23d56bd2db8056764a409cb
Michelle <155619196+michelleChapasuka@users.noreply.github.com>
2025-03-06 13:39:20 +0200

    script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ getComputerChoice(Math.random())
 function getUserChoice(input) {
   
      switch (input) {
-        case input=='paper':
+        case 'paper':
             return input;
         case 'rock':
             return input
@@ -42,9 +42,77 @@ let computerScore=0
 const humanSelection= getUserChoice(userInput)
 const computorSelection= getComputerChoice(Math.random())
 
-console.log(humanSelection);
-console.log(computorSelection);
+// console.log(humanSelection);
+// console.log(computorSelection);
 
+
+function playRound(userChoice,computerChoice) {
+    let playerChoice=userChoice.toLowerCase()
+// when players have a draw
+    if(playerChoice=='rock' && computerChoice=='rock'  
+        ||
+         playerChoice=='paper' && computerChoice=='paper' 
+        || playerChoice=='scissors' && computerChoice=='scissors'){
+
+            console.log(`Player chose: ${playerChoice}`, `computer chose: ${computerChoice}`);
+            console.log('ITS A TIE');  
+             
+            console.log('player score:',userScore);
+            console.log('computer score:',computerScore);
+        }
+
+    
+    
+        
+//  when player wins
+
+    else if(playerChoice=='rock' && computerChoice=='scissors' 
+        ||
+         playerChoice=='paper' && computerChoice=='rock' 
+        || playerChoice=='scissors' && computerChoice=='paper'){
+
+            console.log(`Player chose: ${playerChoice}`, `computer chose: ${computerChoice}`);
+            console.log('YOU WIN!!', playerChoice + ' beats ' + computerChoice); 
+        
+            userScore++
+            console.log('player score;',userScore);
+            console.log('computer score:',computerScore);
+        }
+            
+        
+    // when computer loses
+
+    else if(playerChoice=='scissors' && computerChoice=='rock'  
+             ||
+             playerChoice=='rock' && computerChoice=='paper' 
+            || playerChoice=='paper' && computerChoice=='scissors'){
+
+            console.log(`Player chose: ${playerChoice}`, `computer chose: ${computerChoice}`);
+            console.log('you lose!! ;(', computerChoice + ' beats ' + playerChoice);
+        
+            computerScore++
+            console.log('player score:',userScore);
+            console.log('computer score:', computerScore);
+        }
+            
+        
+        
+        
+        
+}
+playRound(humanSelection,computorSelection)
+
+
+function playGame(round) {
+    let numofRounds=0
+    while (numofRounds<5) {
+    
+        
+        
+    }
+    
+}
+playGame(playRound())
 
 
 
